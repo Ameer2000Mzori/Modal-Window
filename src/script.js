57135ea6dc6bf42dbbeb052cb56023757aae4883
Ameer <ameerapex496@gmail.com>
2023-12-26 19:53:06 +0100

    added more logic , comment , and close card function with logic

--- src/script.js
+++ src/script.js
@@ -2,13 +2,22 @@
 const readMBtns = document.querySelectorAll("#read-More-Btns");
 const shdowOverlay = document.getElementsByClassName("shdow-Overlay")[0];
 const cardBox = document.getElementsByClassName("card-Box")[0];
+const closeCardBtn = document.getElementsByClassName("close-Card-Btn")[0];
 
 // gelobal varibales
 
 // functions
+
+// open card function
 const openCard = () => {
   cardBox.classList.remove("active");
   shdowOverlay.classList.remove("active");
+};
+
+// close card function
+const closeCard = () => {
+  cardBox.classList.add("active");
+  shdowOverlay.classList.add("active");
 };
 
 // event linsters
@@ -17,3 +26,5 @@ readMBtns.forEach((readBtn) => {
     openCard();
   });
 });
+closeCardBtn.addEventListener("click", closeCard);
+// shdowOverlay.addEventListener("click", closeCard);
